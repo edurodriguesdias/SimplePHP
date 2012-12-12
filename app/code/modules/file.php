b468e5002479e8281a8acee3dbc9d79ef2222f39
Rafael Franco <rafaelfranco@me.com>
2012-12-12 16:26:07 -0200

    Envio de arquivos mais genérico

--- app/code/modules/file.php
+++ app/code/modules/file.php
@@ -19,16 +19,17 @@
 			
 		}
 	
-		public function copyFile($source) {
+		public function copyFile($source,$to) {
 			$file = file_get_contents($source);
 			$ext = explode('.', $source);
 
 			$extension = $ext[(count($ext)-1)];
 			$name = rand(0,1000).time();
-			$arq = fopen("../public/upload_files/original/".$name.".".$extension,'a');
+
+			$arq = fopen($to."/".$name.".".$extension,'a');
 			fwrite($arq,$file);
 			fclose($arq);
-			return 'http://'.$_SERVER['HTTP_HOST']."/upload_files/original/".$name.".".$extension;
+			return $name.".".$extension;
 		}
 		
 		public function uploadFile($file,$path) {
@@ -36,10 +37,10 @@
 			$f = explode('.',$file['name']);
 			$ext = $f[count($f)-1];
 			 
-			$name = md5(time()).'.'.$ext;
+			$name = md5(time()).rand(0,1000).'.'.$ext;
 			move_uploaded_file($file["tmp_name"], $path.$name);
 			
-			return $path.$name;
+			return $name;
 		}
 		
 		public function remove($file) {
