0cfd91956c908d17786cc3956825e242581c1ad4
Paulo Cézar <cezarlima05@gmail.com>
2016-07-13 21:44:42 -0300

    ZipArchive - action added
    Exclude file from server..after user download

--- app/code/modules/zip.php
+++ app/code/modules/zip.php
@@ -1,61 +1,52 @@
 <?php
-   	/**
-	 * Project: SimplePHP Framework
-	 * 
-	 * @author Rafael Franco <www.alphacode.com.br>
-	 */
+      /**
+    * Project: SimplePHP Framework
+    * 
+    * @author Rafael Franco <www.alphacode.com.br>
+    */
 
-	/**
-	 * ZipFile Module
-	 *
-	 * @package zip
-	 * @author Paulo Cézar Lima
-	 **/
-	class zip
-	{
-		public function __construct() 
-		{
-			
-		}
+   /**
+    * ZipFile Module
+    *
+    * @package zip
+    * @author Paulo Cézar Lima
+    **/
+   class zip
+   {
+      public function __construct() 
+      {
+         
+      }
 
-		/* creates a compressed zip file */
-		function zip($files = array(),$destination = 'arquivo.zip',$overwrite = false) {
-			//if the zip file already exists and overwrite is false, return false
-			if(file_exists($destination) && !$overwrite) { return false; }
-			//vars
-			$valid_files = array();
-			//if files were passed in...
-			if(is_array($files)) {
-				//cycle through each file
-				foreach($files as $file) {
-					//make sure the file exists
-					if(file_exists($file)) {
-						$valid_files[] = $file;
-					}
-				}
-			}
-			//if we have good files...
-			if(count($valid_files)) {
-				//create the archive
-				$zip = new ZipArchive();
-				if($zip->open($destination,$overwrite ? ZIPARCHIVE::OVERWRITE : ZIPARCHIVE::CREATE) !== true) {
-					return false;
-				}
-				//add the files
-				foreach($valid_files as $file) {
-					$zip->addFile($file,$file);
-				}
-				//debug
-				//echo 'The zip archive contains ',$zip->numFiles,' files with a status of ',$zip->status;
-				// pre($zip);
-				//close the zip -- done!
-				$zip->close();
-				header("Content-type: application/zip"); 
-				header("Content-Disposition: attachment; filename=$destination"); 
-				header("Pragma: no-cache");
-				header("Expires: 0");
-				readfile($destination);
-				exit;
-			}
-		}
+      function zip($files = array(), $destination = 'arquivo.zip',$overwrite = false) {
+         if(file_exists($destination) && !$overwrite) { return false; }
+         $valid_files = array();
+         if(is_array($files)) {
+            foreach($files as $file) {
+               if(file_exists($file)) {
+                  $valid_files[] = $file;
+               }
+            }
+         }
+         if(count($valid_files)) {
+            $zip = new ZipArchive();
+            if($zip->open($path.$destination,$overwrite ? ZIPARCHIVE::OVERWRITE : ZIPARCHIVE::CREATE) !== true) {
+               return false;
+            }
+            foreach($valid_files as $file) {
+               $new_filename = substr($file,strrpos($file,'/') + 1);
+               $zip->addFile($file,$new_filename);
+            }
+            $zip->close();
+            header("Content-type: application/zip"); 
+            header("Content-Disposition: attachment; filename={$destination}"); 
+            header("Pragma: no-cache");
+            header("Expires: 0");
+            readfile($destination);
+            ignore_user_abort(true);
+            unlink($destination);
+            exit;
+         }
+      }
 }
+?>
