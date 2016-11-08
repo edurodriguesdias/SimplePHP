f449cc83ef0bc379c293937a223144a2e8de37f3
Paulo Cézar Lima <cezarlima05@gmail.com>
2016-11-08 12:02:27 -0200

    Função removeAcentos
    Remove qualquar acento, caracter especial ou espaço contido na string,
    muito utilizado em uploader de imagens

--- app/code/modules/util.php
+++ app/code/modules/util.php
@@ -296,4 +296,14 @@
 		$retorno = str_replace(array('R$',' ','.','-','_','(',')',','), '', $valor);
 		return $retorno;
 	}
+
+   // Substitui todos os caracteres especiais pelas suas vogais respectivas
+	function removeAcentos($string) {
+      $pattern = array("'é'", "'è'", "'ë'", "'ê'", "'É'", "'È'", "'Ë'", "'Ê'", "'á'", "'à'", "'ä'", "'â'", "'å'", "'Á'", "'À'", "'Ä'", "'Â'", "'Å'", "'ó'", "'ò'", "'ö'", "'ô'", "'Ó'", "'Ò'", "'Ö'", "'Ô'", "'í'", "'ì'", "'ï'", "'î'", "'Í'", "'Ì'", "'Ï'", "'Î'", "'ú'", "'ù'", "'ü'", "'û'", "'Ú'", "'Ù'", "'Ü'", "'Û'", "'ý'", "'ÿ'", "'Ý'", "'ø'", "'Ø'", "'œ'", "'Œ'", "'Æ'", "'ç'", "'Ç'");
+		$replace = array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E', 'a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A', 'A', 'o', 'o', 'o', 'o', 'O', 'O', 'O', 'O', 'i', 'i', 'i', 'I', 'I', 'I', 'I', 'I', 'u', 'u', 'u', 'u', 'U', 'U', 'U', 'U', 'y', 'y', 'Y', 'o', 'O', 'a', 'A', 'A', 'c', 'C'); 
+
+		$retorno = preg_replace($pattern, $replace, $string);
+		$retorno = str_replace(' ', '_', $retorno);
+      return $retorno;
+   }
 ?>
