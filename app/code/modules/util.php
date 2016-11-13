2b8aa9565900079e155435d6ce6f61100a1215f2
Rafael Franco <rafael@alphacode.com.br>
2016-11-13 09:17:55 -0200

    Calcula a distância

--- app/code/modules/util.php
+++ app/code/modules/util.php
@@ -202,6 +202,24 @@
 		}
 		
 
+		public function calculaDistancia($lat1, $long1, $lat2, $long2) {
+			
+			$d2r = 0.017453292519943295769236;
+
+			$dlong = ($long2 - $long1) * $d2r;
+			$dlat = ($lat2 - $lat1) * $d2r;
+
+			$temp_sin = sin($dlat/2.0);
+			$temp_cos = cos($lat1 * $d2r);
+			$temp_sin2 = sin($dlong/2.0);
+
+			$a = ($temp_sin * $temp_sin) + ($temp_cos * $temp_cos) * ($temp_sin2 * $temp_sin2);
+			$c = 2.0 * atan2(sqrt($a), sqrt(1.0 - $a));
+
+			return 6368.1 * $c;
+			
+		}
+
 	}
 	
 	#developer functions
@@ -306,4 +324,7 @@
 		$retorno = str_replace(' ', '_', $retorno);
       return $retorno;
    }
+
+
+
 ?>
