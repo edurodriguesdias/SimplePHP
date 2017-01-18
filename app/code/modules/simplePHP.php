c880c7165a2a932fb578759d4b49c1273790c03c
Paulo Cézar Lima <cezarlima05@gmail.com>
2017-01-18 16:54:16 -0200

    Adição da tag header_base no core do Simple

--- app/code/modules/simplePHP.php
+++ app/code/modules/simplePHP.php
@@ -111,7 +111,8 @@ class simplePHP extends util {
 		} else {
 			$keys = $control->_actionStart();
 		}
-	   
+        $keys['header_base'] = "<base href=\"http://".$_SERVER['HTTP_HOST']."\">";
+
         #app keys
 
         $html = ($page != '') ? $this->applyKeys($page,$keys) : 'Template not found or empty';
