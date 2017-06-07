e27426b20bea9d579435cd50a70cf330e5abf3a9
Nathalia <nathaliac@Mac-mini-de-Nathalia.local>
2017-06-07 15:37:26 -0300

    params add em options de um select

--- app/code/modules/html.php
+++ app/code/modules/html.php
@@ -4,7 +4,7 @@
 
 	/**
 	 * Project: SimplePHP Framework
-	 * 
+	 *
 	 * @copyright Alphacode  www.alphacode.com.br
 	 * @author Rafael Franco <simplephp@alphacode.com.br>
 	 */
@@ -17,11 +17,11 @@
 	 **/
 	class html extends util
 	{
-		public function __construct() 
+		public function __construct()
 		{
-                    
+
 		}
-		
+
 
 		/**
 		*  Create a html   box
@@ -37,46 +37,49 @@
 		* @param <array> $extraParam
 		* @return select html component <string>
 		*/
-		public function select($full=true,$data=array(),$name='',$selected = 0,$selectOption = 1,$selectLabel = 'Selecione', $cssclass='',$onchange='#',$disabled = false, $extraParam = '' ) 
+		public function select($full=true,$data=array(),$name='',$selected = 0,$selectOption = 1,$selectLabel = 'Selecione', $cssclass='',$onchange='#',$disabled = false, $extraParam = '' )
 		{
 			$return = '';
          if($selectOption == 1) {
 				$return .= "<option value=\"0\">$selectLabel</option>\n";
-			}   
+			}
 
-			if(is_array($data)){
-				foreach($data as $id => $value) {
-					if( $selected == $id ) {
-						$return .= "<option selected value=\"$id\">$value</option>\n";
-					} else {
-						$return .= "<option value=\"$id\">$value</option>\n";
-					}
-				}
-			}
-			$actions = '';
-			if($onchange != '#') {
-				$actions = "onchange=\"$onchange\"";
-			}
 			if($extraParam != '') {
 				foreach ($extraParam as $key => $value) {
 					$params .= ' '.$key.'="'.$value.'"';
 				}
 			}
+
+			if(is_array($data)){
+				foreach($data as $id => $value) {
+					if( $selected == $id ) {
+						$return .= "<option selected value=\"$id\" $params >$value</option>\n";
+					} else {
+						$return .= "<option value=\"$id\" $params >$value</option>\n";
+					}
+				}
+			}
+			
+			$actions = '';
+			if($onchange != '#') {
+				$actions = "onchange=\"$onchange\"";
+			}
+
 			if($full) {
 				if($disabled == true ) {
 					$return = "<select disabled=\"disabled\" class=\"$cssclass\" name=\"$name\" id=\"$name\"  $actions $params >$return</select>\n";
 					} else {
 						$return = "<select class=\"$cssclass\" name=\"$name\" $actions  id=\"$name\" $params >$return</select>\n";
 					}
-				}   
+				}
 				return  $return;
 		}
-		
+
 		/**
 		 * tag function, create an html tag
 		 *
-		 * @return string 
-		 * 
+		 * @return string
+		 *
 		 **/
 		private function tag($tag,$content='',$attributes='')
 		{
@@ -93,59 +96,59 @@
 			}
 				return $return;
 		}
-		
+
 		/**
 		 * div function, create an div
 		 *
-		 * @return string 
-		 * 
+		 * @return string
+		 *
 		 **/
 		public function div($content='',$attributes='')
 		{
-                    
+
 			return $this->tag('div',$content,$attributes);
 		}
-                
+
                 /**
 		 * div function, create an div
 		 *
-		 * @return string 
-		 * 
+		 * @return string
+		 *
 		 **/
 		public function html($content='',$attributes='')
 		{
-                    
+
 			return $this->tag('html',$content,$attributes);
 		}
-                
+
                 /**
 		 * div function, create an div
 		 *
-		 * @return string 
-		 * 
+		 * @return string
+		 *
 		 **/
 		public function header($content='',$attributes='')
 		{
-                    
+
 			return $this->tag('header',$content,$attributes);
 		}
-		
+
 		/**
 		 * b function, create an b
 		 *
-		 * @return string 
-		 * 
+		 * @return string
+		 *
 		 **/
 		public function b($content='',$attributes='')
 		{
 			return $this->tag('b',$content,$attributes);
-		} 
-		
+		}
+
 		/**
 		 * h1 function, create an h1
 		 *
-		 * @return string 
-		 * 
+		 * @return string
+		 *
 		 **/
 		public function h1($content='',$attributes='')
 		{
@@ -154,8 +157,8 @@
 		/**
 		 * h2 function, create an h2
 		 *
-		 * @return string 
-		 * 
+		 * @return string
+		 *
 		 **/
 		public function h2($content='',$attributes='')
 		{
@@ -164,8 +167,8 @@
 		/**
 		 * h3 function, create an h3
 		 *
-		 * @return string 
-		 * 
+		 * @return string
+		 *
 		 **/
 		public function h3($content='',$attributes='')
 		{
@@ -174,19 +177,19 @@
 		/**
 		 * p function, create an p
 		 *
-		 * @return string 
-		 * 
+		 * @return string
+		 *
 		 **/
 		public function p($content='',$attributes='')
 		{
 			return $this->tag('p',$content,$attributes);
 		}
-		
+
 		/**
 		 * span function, create an span
 		 *
-		 * @return string 
-		 * 
+		 * @return string
+		 *
 		 **/
 		public function span($content='',$attributes='')
 		{
@@ -196,8 +199,8 @@
 		/**
 		 * ul function, create an ul
 		 *
-		 * @return string 
-		 * 
+		 * @return string
+		 *
 		 **/
 		public function ul($content='',$attributes='')
 		{
@@ -207,19 +210,19 @@
 		/**
 		 * li function, create an li
 		 *
-		 * @return string 
-		 * 
+		 * @return string
+		 *
 		 **/
 		public function li($content='',$attributes='')
 		{
 			return $this->tag('li',$content,$attributes);
 		}
-		
+
 		/**
 		 * a function, create an a
 		 *
-		 * @return string 
-		 * 
+		 * @return string
+		 *
 		 **/
 		public function link($label,$href='#',$target='',$class='')
 		{
@@ -228,30 +231,30 @@
 			$attributes['class'] = $class;
 			return $this->tag('a',$label,$attributes);
 		}
-		
+
 		/**
 		* img function, create an img
 		*
-		* @return string 
-		* 
+		* @return string
+		*
 		**/
 		public function img($link,$attributes='')
-		{	
+		{
 			$attributes['src'] = $link;
 			return $this->tag('img','',$attributes);
 		}
-		
+
 		/**
 		* table function, create an table
 		*
-		* @return string 
-		* 
+		* @return string
+		*
 		**/
-		public function table($content,$attributes=array('class'=>'contentTable'),$striped=false,$color1='#f8f7ff',$color2='#f6f6f6',$full = true) 
+		public function table($content,$attributes=array('class'=>'contentTable'),$striped=false,$color1='#f8f7ff',$color2='#f6f6f6',$full = true)
 		{
 			 $i = 0;
  		     foreach ($content as $row) {
-				$htmlCol = '';            
+				$htmlCol = '';
 				$x = 0;
 				foreach ($row as $col) {
 					$htmlCol .= $this->tag('td',$col,array('id'=>$attributes['id'].$i.'_col_'.$x,'class'=>$attributes['id'].'_col_'.$x));
@@ -266,48 +269,48 @@
 					$htmlRows .= $this->tag('tr',$htmlCol,array('id'=>$attributes['id'].'_row_'.$i,'bgcolor'=>$color));
 				} else {
 					$htmlRows .= $this->tag('tr',$htmlCol,array('id'=>$attributes['id'].'_row_'.$i));
-				}  
-				
+				}
+
 				if($i == 0) {
-				   $htmlRows =  $this->tag('thead', $htmlRows );             
+				   $htmlRows =  $this->tag('thead', $htmlRows );
 				}
 				$i++;
  		     }
  		    if($full) {
- 		    	$return = $this->tag('table',$htmlRows,$attributes);	
+ 		    	$return = $this->tag('table',$htmlRows,$attributes);
  		    } else {
  		    	$return = $htmlRows;
  		    }
  		    return $return;
-			
+
 		}
-		
+
 		/**
 		* input function, create an input
 		*
-		* @return string 
-		* 
+		* @return string
+		*
 		**/
 		public function input($type,$value,$attributes='')
-		{	
+		{
 			$attributes['value'] = $value;
 			$attributes['type'] = $type;
 			return $this->tag('input','',$attributes);
-		} 
-		
+		}
+
 		/**
 		* input function, create an input
 		*
-		* @return string 
-		* 
+		* @return string
+		*
 		**/
 		public function textArea($value,$attributes='')
-		{	
+		{
 			return $this->tag('textArea', $value,$attributes);
 		}
-		
 
-		
+
+
 
 } // END class html
 
